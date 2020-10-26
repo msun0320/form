3a724dfb6cd26a2f195b390e1a7da604905aa146
Mingyue Sun <mingyueees@gmail.com>
2020-10-26 04:50:04 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Hotel Reservation Form
+
+A registration form with client-side validation. [Live Demo](https://msun0320.github.io/form/)
+
+## Built With
+
+* HTML
+* CSS
+* JavaScript
---
+++ script.js
@@ -0,0 +1,37 @@
+const button = document.querySelector('button');
+const form = document.querySelector('form');
+const errorList = document.querySelector('section.error ul');
+let errors = [];
+
+const addError = () => {
+	const inputs = document.querySelectorAll('input');
+
+	inputs.forEach(element => {
+		const labels = Array.from(document.querySelectorAll('label'));
+		const label = labels.find(item => item.htmlFor === element.id).textContent;
+
+		if (element.validity.valueMissing) {
+			errors.push(`The ${label} field is required.`);
+		} else if (element.validity.patternMismatch || element.validity.rangeUnderflow) {
+			errors.push(`${label} is not valid.`);
+		}
+	});
+};
+
+const showError = () => {
+	addError();
+
+	errors.forEach(element => {
+		const errorElement = document.createElement('li');
+
+		errorElement.textContent = element;
+		errorList.appendChild(errorElement);
+	});
+};
+
+button.addEventListener('click', () => {
+	errorList.textContent = '';
+	errors = [];
+	form.classList.add('submitted');
+	showError();
+});
